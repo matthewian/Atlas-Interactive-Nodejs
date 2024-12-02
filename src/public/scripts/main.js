26782b09f1ecb563f25121cd5c17d661f64fd3d3
mattweeks <mweeks@onenorth.com>
2024-12-02 13:06:04 -0600

    developer updates

--- src/public/scripts/main.js
+++ src/public/scripts/main.js
@@ -1,3 +1,18 @@
-// import Alpine from 'alpinejs'
-// window.Alpine = Alpine
-// Alpine.start()
+window.onload = () => {
+    // const isLightMode = localStorage.getItem("is-light-mode")
+    // if (isLightMode && isLightMode === "true") {
+    //     setLightMode()
+    // }
+
+    const darkModeToggle = document.getElementById("dark-mode-toggle")
+    darkModeToggle.addEventListener("change", () =>  {
+        document.body.classList.toggle("light-mode")
+        //localStorage.setItem("is-light-mode", darkModeToggle.checked ? "true" : "false")
+    })
+
+    // const setLightMode = () => {
+    //     darkModeToggle.checked = true
+    //     document.body.classList.toggle("light-mode")
+    //     localStorage.setItem("is-light-mode", "true")
+    // }
+}
